55c8286777749165a0ecfac9a90f724ff9525fa1
Lucy Armitage <la724@imperial.ac.uk>
2025-06-18 23:34:22 +0100

    create dictionary .jl

---
+++ Dictionary/create_dict.jl
@@ -0,0 +1,99 @@
+using CUDA, KomaMRI, MAT, Suppressor, JLD2, FileIO, LinearAlgebra
+CUDA.set_runtime_version!(v"12.0")
+
+out_folder = "progress_SP_test"
+out_file   = "progress_SP_test.mat"
+
+f_idx = matopen("D_IDX_SP_Phantom2025.mat")
+idx = read(f_idx, "idx")
+close(f_idx)
+
+f_dict = matopen("D_Phantom2025_invEff096_SPinf_norefocusingTEadj_576InvTime_1000RF_10mm_101iso_0.mat")
+dict_epg = read(f_dict, "dict0")
+close(f_dict)
+
+sampled_pairs_ms = [(row[1], row[2]) for row in eachrow(idx)]
+sampled_pairs_s  = [(T1 / 1000, T2 / 1000) for (T1, T2) in sampled_pairs_ms]
+
+sys = Scanner()
+
+sim_params = KomaMRICore.default_sim_params()
+sim_params["return_type"] = "mat"
+sim_params["sim_method"] = BlochDict(save_Mz=true)
+sim_params["gpu"] = true
+
+seq = read_seq("mpf_001_short.seq")
+
+function simulate_slice_profile_x(
+    seq::Sequence; x=range(-10e-3, 10e-3, length=1001), sim_params=Dict{String,Any}("Δt_rf" => 1e-6)
+)
+    sim_params["return_type"] = "state"
+    sys = Scanner()
+    obj = Phantom{Float64}(; x=Array(x), z=zeros(size(x)))
+    mag = simulate(obj, seq, sys; sim_params)
+    return mag
+end
+
+x_pos = collect(range(-10e-3, 10e-3, length=1001))
+M_rf = simulate_slice_profile_x(seq[1035]; x=x_pos)
+profile = abs.(M_rf.xy)
+profile ./= sum(profile)
+
+for (i, (T1, T2)) in enumerate(sampled_pairs_s)
+    key = (Int(round(T1 * 1000)), Int(round(T2 * 1000)))
+    # filepath = "progress_SP/signal_$(key[1])_$(key[2]).jld2"
+    filepath = joinpath(out_folder, "signal_$(key[1])_$(key[2]).jld2")
+
+    if isfile(filepath)
+        if i % 50 == 0
+            println("Skipping existing signal for $key (iteration $i)")
+            flush(stdout)
+        end
+        continue
+    end
+
+    obj = Phantom{Float64}(
+        x = x_pos,
+        y = zeros(length(x_pos)),
+        z = zeros(length(x_pos)),
+        T1 = fill(T1, length(x_pos)),
+        T2 = fill(T2, length(x_pos))
+    )
+
+    sig = nothing
+    @suppress sig = simulate(obj, seq, sys; sim_params=sim_params)
+
+    sig_clean = dropdims(sig, dims=4)
+    sig_channel1 = sig_clean[:, :, 1]
+
+    weighted_signal = sig_channel1 * profile
+    signal_mag = abs.(weighted_signal)
+    signal_mag = signal_mag[124:248:end][1:1000]
+    signal_mag ./= norm(signal_mag)
+
+    @save filepath key signal_mag
+
+    if i % 50 == 0 || i == length(sampled_pairs_s)
+        println("Processed $i / $(length(sampled_pairs_s)) entries")
+        flush(stdout)
+    end
+end
+
+#files = readdir("progress_SP")
+files = readdir(out_folder)
+num_entries = length(files)
+timepoints = 1000
+
+bloch_matrix = zeros(Float64, timepoints, num_entries)
+idx_bloch = zeros(Float64, num_entries, 2)
+
+for (j, file) in enumerate(files)
+    # filepath = joinpath("progress_SP", file)
+    filepath = joinpath(out_folder, file)
+    @load filepath key signal_mag
+    bloch_matrix[:, j] .= signal_mag
+    idx_bloch[j, :] .= key
+end
+
+# matwrite("progress_SP.mat", Dict("dict0" => bloch_matrix, "idx" => idx_bloch))
+matwrite(out_file, Dict("dict0" => bloch_matrix, "idx" => idx_bloch))
