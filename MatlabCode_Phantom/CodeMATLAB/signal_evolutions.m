f4ee90608562bf66918039e490ade49e92c7b7c4
Lucy Armitage <lucy.armitage1@btinternet.com>
2025-07-18 18:17:59 +0100

    fix axis titles

--- MatlabCode_Phantom/CodeMATLAB/signal_evolutions.m
+++ MatlabCode_Phantom/CodeMATLAB/signal_evolutions.m
@@ -1,8 +1,14 @@
-folder = 'C:/Users/lucya/MSC_PROJECT/MatlabCode_Phantom/recon_results';
-load(fullfile(folder, '20mm_101.mat'));
+clear;
 
-x = 77; 
-y = 53;  
+folder = 'C:/Users/lucya/MSC_PROJECT/MatlabCode_Phantom/recon_results';
+load(fullfile(folder, 'new_10mm_101.mat'));
+
+x_flipped = 63; 
+y_flipped = 97;  
+
+x = N - x_flipped + 1;  % reverse fliplr
+y = N - y_flipped + 1;  % reverse flip
+
 pixel_idx = sub2ind([N, N], y, x);
 
 T1 = idx(idx2(pixel_idx), 1);
@@ -14,8 +20,8 @@ compressed_meas_sig = squeeze(svd_images(y, x, :));
 meas_sig = D.u * compressed_meas_sig;
 
 % Dictionary signal
-compressed_dict_sig = D.magnetization(:, idx2(pixel_idx));
-dict_sig = dict0(:, idx2(pixel_idx));
+compressed_dict_sig = conj(D.magnetization(:, idx2(pixel_idx)));
+dict_sig = conj(dict0(:, idx2(pixel_idx)));
 
 % Scaling 
 scaling_factor_compr = norm(compressed_meas_sig) / norm(compressed_dict_sig);
@@ -32,21 +38,21 @@ figure;
 plot(abs(compressed_meas_sig), 'k--'); hold on;
 plot(abs(compressed_dict_sig_scaled), 'r');
 legend('Measured signal', "Matched dictionary signal (scaled)")
-xlabel('Timepoint'); ylabel('|Signal|');                                       % Fix axis labels !!
+xlabel('Singular vector index'); ylabel('|Signal|');                                       % Fix axis labels !!
 title(sprintf('Abs - Voxel (%d, %d): T1=%d, T2=%d', x, y, T1, T2));
 
 figure;
 plot(real(compressed_meas_sig), 'k--'); hold on;
 plot(real(compressed_dict_sig_scaled), 'r');
 legend('Measured signal', "Matched dictionary signal (scaled)")
-xlabel('Timepoint'); ylabel('|Signal|');                                       % Fix axis labels !!
+xlabel('Singular vector index'); ylabel('Signal real part');                                       % Fix axis labels !!
 title(sprintf('Real - Voxel (%d, %d): T1=%d, T2=%d', x, y, T1, T2));
 
 figure;
 plot(imag(compressed_meas_sig), 'k--'); hold on;
 plot(imag(compressed_dict_sig_scaled), 'r');
 legend('Measured signal', "Matched dictionary signal (scaled)")
-xlabel('Timepoint'); ylabel('|Signal|');                                       % Fix axis labels !!
+xlabel('Singular vector index'); ylabel('Signal imag part');                                       % Fix axis labels !!
 title(sprintf('Imag - Voxel (%d, %d): T1=%d, T2=%d', x, y, T1, T2));
 
 % Uncompressed signal 
@@ -62,13 +68,17 @@ figure;
 plot(real(meas_sig), 'k--'); hold on;
 plot(real(dict_sig_scaled), 'r');
 legend('Measured signal', 'Matched dictionary signal (scaled)');
-xlabel('Timepoint'); ylabel('|Signal|');
+xlabel('Timepoint'); ylabel('Signal real part');
 title(sprintf('Real - Voxel (%d, %d): T1=%d, T2=%d', x, y, T1, T2));
 
 figure;
 plot(imag(meas_sig), 'k--'); hold on;
 plot(imag(dict_sig_scaled), 'r');
 legend('Measured signal', 'Matched dictionary signal (scaled)');
-xlabel('Timepoint'); ylabel('|Signal|');
+xlabel('Timepoint'); ylabel('Signal imag part');
 title(sprintf('Imag - Voxel (%d, %d): T1=%d, T2=%d', x, y, T1, T2));
 
+
+
+% filename = sprintf('measured_signal_vial1_x%d_y%d.mat', x_flipped, y_flipped);
+% save(filename, 'meas_sig');
