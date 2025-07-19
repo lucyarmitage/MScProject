0c0945091d5c9ed00db184f3b918c36855810e43
Lucy Armitage <lucy.armitage1@btinternet.com>
2025-07-19 10:31:28 +0100

    interactive clicking

--- MatlabCode_Phantom/CodeMATLAB/extract_t1t2.m
+++ MatlabCode_Phantom/CodeMATLAB/extract_t1t2.m
@@ -1,33 +1,34 @@
-% --- Load precomputed reconstruction and dictionary ---
-folder = 'C:/Users/lucya/MSC_PROJECT/MatlabCode_Phantom/';
+clear;
+
+folder = 'C:/Users/lucya/MSC_PROJECT/MatlabCode_Phantom/recon_results';
 addpath(genpath('C:/Users/lucya/MSC_PROJECT/MatlabCode_Phantom/CodeMATLAB/utils'));
-load(fullfile(folder, 'recon_results101.mat'));
-load('vial_mask_geometry.mat');  % loads 'centres' and 'radii'
+load(fullfile(folder, 'new_10mm_501_short.mat'));
+load('vial_mask_geometry.mat');  % centres, radii
 
-% Qmaps = reshape(D.lookup_table(idx2,:),[[N N 1], size(D.lookup_table,2)]);
-% Qmaps = cat(numel(size(Qmaps)),Qmaps,reshape(c ./ D.normalization(idx2).',[N N]));
-% % PD    = c ./ D.normalization(idx2).';
-% % PD    = reshape(PD, [N N]);
-% 
-% % Do some background masking
-% mask = sum(abs(reshape(svd_images,[[N N] R])),3);
-% mask(mask < .4* max(mask(:))) = 0;
-% mask (mask ~= 0) = 1;
-% Qmaps = bsxfun(@times,Qmaps,mask);
-% 
-% Qmaps = flip (Qmaps);
-% % figure; imshow3(fliplr(Qmaps(:,:,1,1)), [0 max(max(Qmaps(:,:,1,1)))])
-% % figure; imshow3(fliplr(Qmaps(:,:,1,2)), [0 max(max(Qmaps(:,:,1,2)))])
-% % max(max(Qmaps(:,:,1,1)))
-% % max(max(Qmaps(:,:,1,2)))
-% 
-% figure; imshow3(fliplr(Qmaps(:,:,1,1)), [0 3000]);
-% figure; imshow3(fliplr(Qmaps(:,:,1,2)), [0 1500]); 
-% % figure; imshow3(fliplr(Qmaps(:,:,1,1)), [0 3000]); colormap hot;
-% % figure; imshow3(fliplr(Qmaps(:,:,1,2)), [0 1500]); colormap turbo;
+Qmaps = reshape(D.lookup_table(idx2,:),[[N N 1], size(D.lookup_table,2)]);
+Qmaps = cat(numel(size(Qmaps)),Qmaps,reshape(c ./ D.normalization(idx2).',[N N]));
+% PD    = c ./ D.normalization(idx2).';
+% PD    = reshape(PD, [N N]);
 
-magImg = fliplr(Qmaps(:,:,1,1))
-  % Flip vertically to match Qmaps
+% Do some background masking
+mask = sum(abs(reshape(svd_images,[[N N] R])),3);
+mask(mask < .4* max(mask(:))) = 0;
+mask (mask ~= 0) = 1;
+Qmaps = bsxfun(@times,Qmaps,mask);
+
+Qmaps = flip (Qmaps);
+% figure; imshow3(fliplr(Qmaps(:,:,1,1)), [0 max(max(Qmaps(:,:,1,1)))])
+% figure; imshow3(fliplr(Qmaps(:,:,1,2)), [0 max(max(Qmaps(:,:,1,2)))])
+% max(max(Qmaps(:,:,1,1)))
+% max(max(Qmaps(:,:,1,2)))
+
+figure; imshow3(fliplr(Qmaps(:,:,1,1)), [0 3000]);
+figure; imshow3(fliplr(Qmaps(:,:,1,2)), [0 1500]); 
+% figure; imshow3(fliplr(Qmaps(:,:,1,1)), [0 3000]); colormap hot;
+% figure; imshow3(fliplr(Qmaps(:,:,1,2)), [0 1500]); colormap turbo;
+
+
+magImg = fliplr(Qmaps(:,:,1,1));
 [H, W] = size(magImg);
 [xx, yy] = meshgrid(1:W, 1:H);
 
@@ -40,7 +41,6 @@ for k = 1:nVials
     r  = radii(k);
     Masks(:,:,k) = ((xx - cx).^2 + (yy - cy).^2) <= r^2;
 end
-
 
 
 T1 = fliplr(Qmaps(:,:,1,1));
@@ -70,4 +70,60 @@ for k = 1:nVials
          'HorizontalAlignment','center');
 end
 hold off;
+
+figure; 
+hImg = imagesc(magImg); axis image; colormap hot; colorbar;
+title('Click on pixel to view T1/T2 values');
+
+dcm = datacursormode(gcf);
+set(dcm, 'Enable', 'on', ...
+         'DisplayStyle', 'datatip', ...
+         'SnapToDataVertex', 'on', ...
+         'UpdateFcn', {@myTooltipFunction, T1, T2});
+
+
+disp('Click');
+
+
+function txt = myTooltipFunction(~, event_obj, T1, T2)
+    pos = get(event_obj, 'Position');
+    x = round(pos(1));
+    y = round(pos(2));
+
+    if y >= 1 && y <= size(T1, 1) && x >= 1 && x <= size(T1, 2)
+        t1_val = T1(y, x);
+        t2_val = T2(y, x);
+        txt = {
+            ['X: ', num2str(x)], ...
+            ['Y: ', num2str(y)], ...
+            ['T1: ', num2str(t1_val, '%.1f'), ' ms'], ...
+            ['T2: ', num2str(t2_val, '%.1f'), ' ms']
+        };
+    else
+        txt = {'Out of bounds'};
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% --- Pick a pixel and report its index ---
+[x_click, y_click] = ginput(1);
+x = round(x_click);
+y = round(y_click);
+pixel_idx = sub2ind([N, N], y, x);  % Make sure N matches actual image size
+
+fprintf('You selected pixel (x = %d, y = %d)\n', x, y);
+fprintf('Linear index (pixel_idx) = %d\n', pixel_idx);
 
